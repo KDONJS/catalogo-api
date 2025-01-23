97c99c55d6aafae3ce9cfa8baeb2b4007de8ec6f
Yorlin Quispe <80475432+KDONJS@users.noreply.github.com>
2025-01-23 01:14:21 -0500

    test

--- iac/eks.tf
+++ iac/eks.tf
@@ -55,8 +55,6 @@ resource "aws_eks_node_group" "eks_nodes" {
   node_group_name = "eks-nodes"
   node_role_arn   = aws_iam_role.eks_node_role.arn
   subnet_ids      = [aws_subnet.public.id, aws_subnet.private.id]
-
-  instance_types = ["t3.medium"]
   
   scaling_config {
     desired_size = 2
