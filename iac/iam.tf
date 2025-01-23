776cc4a54ce36380424174f335137af90ac0949f
Yorlin Quispe <80475432+KDONJS@users.noreply.github.com>
2025-01-23 16:54:24 -0500

    test

--- iac/iam.tf
+++ iac/iam.tf
@@ -18,3 +18,33 @@ resource "aws_iam_role" "ec2_role" {
     Name = "EC2Role"
   }
 }
+
+resource "aws_iam_policy" "ec2_policy" {
+  name        = "ec2_policy"
+  description = "Política para acceso a S3 y CloudWatch"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:*",
+          "logs:*"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ec2_role_attachment" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = aws_iam_policy.ec2_policy.arn
+}
+
+# 🔹 Asegurar que este recurso existe
+resource "aws_iam_instance_profile" "ec2_instance_profile" {
+  name = "ec2_instance_profile"
+  role = aws_iam_role.ec2_role.name
+}
